beedf4a674d0f9e6769f95921dd2befe22357089
0253_Napapat Pannasin <napapat0564@gmail.com>
2026-02-02 14:32:36 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World');
+app.get('/', (req, res) => {
+  res.send('Backend is running');
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
