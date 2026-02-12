228bddb1f6bc43a3864a6b4cda87745d9a57aa3e
ัYadawadee Klinsmith <72minniemy@gmail.com>
2026-02-12 18:41:25 +0700

    update

---
+++ database/migrations/009_create_doctors.sql
@@ -0,0 +1,10 @@
+CREATE TABLE doctors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    doctor_name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO doctors (doctor_name)
+VALUES
+('นายแพทย์ณัฐวิทย์ โนวังหาร'),
+('นายแพทย์ธนภัทร ธนศรีสถิตย์');
+
